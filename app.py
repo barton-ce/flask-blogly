adde6af50f966dde7aadfcc252c115d58fb0b3ef
barton-ce <99145883+barton-ce@users.noreply.github.com>
2023-03-17 14:58:24 -0500

    Change

--- app.py
+++ app.py
@@ -1,94 +1,97 @@
-import unittest
-from app import app
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'supersecret'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = 'ihaveasecret'
 
-# Stop Flask-DebugToolbar from running in tests
-app.config['TESTING'] = True
-app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
 
 connect_db(app)
 db.create_all()
 
 
-class UserViewsTestCase(unittest.TestCase):
-    
+@app.route('/')
+def root():
 
-    def setUp(self):
-        
+    return redirect("/users")
 
-        User.query.delete()
 
-        user = User(first_name='John', last_name='Doe', image_url='http://example.com')
-        db.session.add(user)
-        db.session.commit()
+@app.route('/users')
+def users_index():
+    """Show a page with info on all users"""
 
-        self.user_id = user.id
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
 
-        self.client = app.test_client()
-        app.config['TESTING'] = True
 
-    def tearDown(self):
-        
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
 
-        db.session.rollback()
+    return render_template('users/new.html')
 
-    def test_users_index(self):
-        
 
-        with self.client as c:
-            resp = c.get('/users')
-            html = resp.get_data(as_text=True)
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Handle form submission for creating a new user"""
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('John Doe', html)
-            self.assertIn('http://example.com', html)
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
 
-    def test_new_user_form(self):
-        
+    db.session.add(new_user)
+    db.session.commit()
 
-        with self.client as c:
-            resp = c.get('/users/new')
-            html = resp.get_data(as_text=True)
+    return redirect("/users")
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h1>Create a user</h1>', html)
 
-    def test_show_user(self):
-        
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show a page with info on a specific user"""
 
-        with self.client as c:
-            resp = c.get(f'/user/{self.user_id}')
-            html = resp.get_data(as_text=True)
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h1>John Doe</h1>', html)
-            self.assertIn('http://example.com', html)
 
-    def test_edit_user(self):
-        
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show a form to edit an existing user"""
 
-        with self.client as c:
-            resp = c.get(f'/users/{self.user_id}/edit')
-            html = resp.get_data(as_text=True)
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn('<h1>Edit John Doe</h1>', html)
-            self.assertIn('http://example.com', html)
 
-    def test_users_destroy(self):
-        
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Handle form submission for updating an existing user"""
 
-        with self.client as c:
-            resp = c.post(f'/users/{self.user_id}/delete')
-            self.assertEqual(resp.status_code, 302)
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
 
-            user = User.query.get(self.user_id)
-            self.assertIsNone(user)
+    db.session.add(user)
+    db.session.commit()
 
-if __name__ == '__main__':
-    unittest.main()
+    return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
